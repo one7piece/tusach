e664b6bbce07d882337d2bed5de2c1b090a6536e
one7piece <one7piece@gmail.com>
2016-01-25 10:26:37 +1100

    add script to build parser

---
+++ server/build-parsers.sh
@@ -0,0 +1,4 @@
+export parser_ipath=~/home/pi/dev/tusach/server/src/dv.com.tusach/parser
+export parser_opath=~/home/pi/webserver/library/parser
+
+go build -o $parser_opath/truyenyy $parser_ipath/truyenyy/truyenyy.go
