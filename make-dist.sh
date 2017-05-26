9d7ec607b76a9071cfcfc292ed73341b3659a382
one7piece <one7piece@gmail.com>
2017-05-26 01:27:02 +0100

    update

--- make-dist.sh
+++ make-dist.sh
@@ -16,8 +16,10 @@ if [ $? -ne 0 ]; then exit 1; fi
 ./build-parsers.sh $1
 if [ $? -ne 0 ]; then exit 1; fi
 
-./make-client.sh update
+echo "Building client..."
+./make-client.sh 
 
+echo "Creating tar file..."
 chmod +x dist/library/make-epub.sh
 chmod +x dist/library/parser/*
 tar -czvf tusach.tar.gz dist --exclude=tusach-linux --exclude=tusach.ql
