cb0779d2fde04892bb0777a8f66044b83d1e25e0
one7piece <one7piece@gmail.com>
2019-11-02 19:38:31 +1100

    update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';   // NgModel lives here
 // import material modules
-import { MatToolbarModule, MatInputModule, MatListModule, MatExpansionModule, 
-  MatFormFieldModule, MatButtonModule, MatMenuModule, MatSidenavModule, 
-  MatIconModule, MatCardModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './interceptors';
